25187f2fa67c36ffb6b439ef0df4999580536100
João Pedro Pacheco <jppn4430@gmail.com>
2023-04-18 16:52:21 -0300

    pronto

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prova01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (VerificarExpressao(textBox1.Text))
+            {
+                label1.Text = "Expressão " +textBox1.Text +  " está correta!";
+                textBox1.Text = "";
+            }
+            else
+            {
+                label1.Text = "Expressão " +textBox1.Text +  " está errada!!";
+                textBox1.Text = "";
+            }
+
+
+            textBox1.Text = "";
+
+        }
+
+        public static bool VerificarExpressao(string expressao)
+        {
+            Pilha pilha = new Pilha();
+
+            foreach (char caractere in expressao)
+            {
+                if (caractere == '(' || caractere == '[' || caractere == '{')
+                {
+                    pilha.Empilhar(caractere);
+                }
+                else if (caractere == ')' || caractere == ']' || caractere == '}')
+                {
+                    if (pilha.EstaVazia())
+                    {
+                        return false;
+                    }
+
+                    char topo = pilha.Desempilhar();
+
+                    if (caractere == ')' && topo != '(' ||
+                        caractere == ']' && topo != '[' ||
+                        caractere == '}' && topo != '{')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return pilha.EstaVazia();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
---
+++ Pilha.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova01
+{
+    internal class Pilha
+    {
+        NoPilha topo;
+
+        public Pilha()
+        {
+            topo = null;
+        }
+
+        public void Empilhar(char valor)
+        {
+            NoPilha novo = new NoPilha(topo, valor);
+            topo = novo;
+        }
+
+        public char Desempilhar()
+        {
+            if (EstaVazia())
+            {
+                throw new Exception("A pilha está vazia!");
+            }
+            else
+            {
+                char valor = topo.Simbolo;
+                topo = topo.Prox;
+                return valor;
+            }
+        }
+
+        public char Topo()
+        {
+            if (EstaVazia())
+            {
+                throw new Exception("A pilha está vazia!");
+            }
+            return topo.Simbolo;
+        }
+
+        public bool EstaVazia()
+        {
+            return topo == null;
+        }
+
+
+    }//end
+}
